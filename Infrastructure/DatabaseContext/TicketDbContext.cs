21793a5dc994f9a9c79cffdd52aba8a590197cb4
Matheus de Souza Melo <matheus.melo@vortigo.com.br>
2023-09-08 16:15:20 -0300

    inhjn

---
+++ Infrastructure/DatabaseContext/TicketDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure.DatabaseContext
+{
+    public class TicketDbContext : DbContext
+    {
+        readonly string ticketsConnectionString = string.Empty;
+        public TicketDbContext(IConfiguration configuration)
+        {
+            ticketsConnectionString = configuration.GetConnectionString("tickets") ?? string.Empty;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(ticketsConnectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
